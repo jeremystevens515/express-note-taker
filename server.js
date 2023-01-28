0c133202dd929a5584bc650378970344d2f3b1cd
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-01-27 20:35:58 -0500

    get requests working

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const express = require('express');
+const path = require('path');
 const fs = require('fs');
 const db = require('./db/db.json');
 
 const app = express();
 const PORT = 3001;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+// defines homepage
 app.get('/', (req, res) => {
-    res.sendFile(__dirname,'./public/index.html');
-})
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+});
+// defines notes.html
+app.get('/notes', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
+// API routes
+app.get('/api/notes', (req, res) => {
+    res.json(db);
+});
+
+// app.post('/api/notes', (req, res) => {
+
+// });
+
+// activates server on localhost:PORT
 app.listen(PORT, () => {
     console.log(`Express server is listening on port ${PORT}`)
 });
