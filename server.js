f21c1ce96bc2f86b1dc0f5e04c6492b2ef52291b
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-01-27 21:30:59 -0500

    added post request function for /api/notes path in server.js. added random number generator to generate a 5 number note_id. fs.appendFile() works to send stringified json data to db.json. however, i need to figure out a way to format the json file in such a way that all of the objects are INSIDE of an array. perhaps i need to read existing data back and then rewrite the whole file. sounds inefficient, but i don't know of another way.

--- server.js
+++ server.js
@@ -25,9 +25,33 @@ app.get('/api/notes', (req, res) => {
     res.json(db);
 });
 
-// app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
+    // res.send(`${req.method} request received!`)
+    console.log(`${req.method} request received!`)
 
-// });
+    // random number generator to get a string of 5 random numbers to use as the id
+    let id = [];
+    for (let i = 0; i < 5; i++) {
+        id.push(Math.floor(Math.random()* 10));
+    };
+
+    // this sets the first property of req.body as the value of title, and the second as text
+    const { title, text} = req.body
+
+    // create a new object to be sent back to the front-end and saved in db.json
+    const newNote = {
+        title,
+        text,
+        note_id: id.join("")
+    };
+
+    fs.appendFile('./db/db.json', JSON.stringify(newNote), (err) => {
+        err ? console.error(err) : console.log("new note appended successfully!")
+    });
+    
+    res.json(newNote);
+    console.log(newNote)
+});
 
 // activates server on localhost:PORT
 app.listen(PORT, () => {
